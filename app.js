27a880a5e67193b9f265444700907cf2bc65c884
Antoni <antonig@sas.upenn.edu>
2019-03-27 03:12:41 -0400

    readme+

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const grailed_interface = require('./models/grailed_interface.js');
 const copy_file = require('./utils/file_overwrite.js');
 const readline = require('readline');
+const log = require('debug')('cli');
 
 const input = readline.createInterface({
   input: process.stdin,
@@ -18,10 +19,12 @@ What would you like to do? Enter the corresponding number:
 
 `
 // initial prompt
+log('prompting initial question');
 input.question(question,processAnswer);
 
 // process the answer
 function processAnswer(answer) {
+  log('prompted initial question, handling response: '+answer);
   console.log();
   // handle each question appropriately
   switch(answer.trim()) {
@@ -55,6 +58,7 @@ function processAnswer(answer) {
 
 function dryPrompt(tgt) {
   input.question('Is this a dry run? (no changes will be committed to the database). Input y/n: \n', (answer) => {
+    log('dry run prompted, handling answer : '+answer);
     switch(answer.trim()) {
       case 'y':
         if (tgt === 'username') { resolveDuplicates(true); }
@@ -77,7 +81,8 @@ function dryPrompt(tgt) {
 function outputDisallowed() {
   grailed_interface.disallowed_usernames('./db/grailed-exercise-live.sqlite3', (err,res) => {
       if (err) {
-        console.log('An error occured: ' + err);
+        console.log('An error occured processing this question.');
+        log('Error outputting for outputDisallowed: '+err);
       }
       else {
         outputRows(res);
@@ -88,7 +93,8 @@ function outputDisallowed() {
 function resolveDuplicates(dry) {
   grailed_interface.collision_resolution('./db/grailed-exercise-live.sqlite3', dry, (err,res) => {
       if (err) {
-        console.log('An error occured: ' + err);
+        console.log('An error occured processing this question.');
+        log('Error outputting for resolveDuplicates: '+err);
       }
       else {
         outputRows(res);
@@ -99,7 +105,8 @@ function resolveDuplicates(dry) {
 function resolveDisallowed(dry) {
   grailed_interface.disallowed_resolution('./db/grailed-exercise-live.sqlite3', dry, (err,res) => {
       if (err) {
-        console.log('An error occured: ' + err);
+        console.log('An error occured processing this question.');
+        log('Error outputting for resolveDisallowed: '+err);
       }
       else {
         outputRows(res);
@@ -110,10 +117,12 @@ function resolveDisallowed(dry) {
 function resetDB() {
   copy_file.copy_file('./db/grailed-exercise.sqlite3','./db/grailed-exercise-live.sqlite3', (err,res) => {
     if (err) {
-      console.log('There was an error reseting the database: ' + err);
+      console.log('There was an error reseting the database.');
+      log('Error resetting db: '+err);
     }
     else {
       console.log('Successfully reset the database.');
+      log('closed the db, closing with no errors.');
     }
     process.exit();
   });
@@ -132,5 +141,6 @@ function outputRows(input) {
     console.log('  '+el.id +' \t| ' + el.username);
   });
   // reaching the end of this method means the function has finished;
+  log('output achieved, closing with no errors.')
   process.exit()
 }
